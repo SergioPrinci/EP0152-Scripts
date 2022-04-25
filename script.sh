1183c15f7f3c4cd6be7cf7d4fec6371c5efeceb8
SergioPrinci <sergioprinci03@live.com>
2022-04-25 22:19:10 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 echo "Script to install oled and leds python scripts"
 
 tmpFolder="/tmp"
 bbioLink="https://github.com/adafruit/adafruit-beaglebone-io-python.git"
-oledLink="https://github.com/SergioPrinci/EP0152-Scripts.git"
+scriptsLink="https://github.com/SergioPrinci/EP0152-Scripts.git"
 
-if [ $EUID -ne 0 ]
+if [ "$EUID" -ne 0 ]
 then
         echo "Please run as root"
         exit
@@ -16,7 +16,7 @@ then
         echo "Pip found!"
 else
         echo "Pip not found, installing it..."
-        apt install python3-pip
+        apt install python3-pip -y
 fi
 
 if $(git --version > /dev/null);
@@ -24,7 +24,7 @@ then
         echo "Git found!"
 else
         echo "Git not found, installing it..."
-        apt install git
+        apt install git -y
 fi
 
 echo "Making sure you enabled I2C..."
@@ -37,25 +37,26 @@ else
 fi
 
 echo "Installing pip libraries..."
-python3-pip install setuptools wheel -q
-python3-pip install pi-ina219 -q
-python3-pip install Adafruit-SSD1306 -q
+pip install setuptools wheel -q
+pip install pi-ina219 -q
+pip install Adafruit-SSD1306 -q
+pip install smbus Pillow -q
 
 echo "Cloning BBIO repo..."
 cd $tmpFolder
 git clone $bbioLink -q
 cd adafruit-beaglebone-io-python
-python3 setup.py install
+python3 setup.py -q install
 
 echo "Downloading scripts..."
 git clone $scriptsLink -q
-cd RPiFEBP
+cd EP0152-Scripts
 mkdir $HOME/Scripts
-mv oled.py $HOME/Scripts/*.py
+mv *.py $HOME/Scripts
 
 echo "Modifying rc.local..."
 rm /etc/rc.local
-echo "#!/bin/sh -e\npython $HOME/Scripts/leds.py\npython $HOME/Scripts/oled.py\nexit" > /etc/rc.local
+echo -e "#!/bin/sh -e\npython3 $HOME/Scripts/leds.py\npython3 $HOME/Scripts/oled.py\nexit" > /etc/rc.local
 chmod +x /etc/rc.local
 
 echo "All done!"
