d94d7ddb4446acbf9a882384986aa66dc003ab51
SergioPrinci <sergioprinci03@live.com>
2022-04-25 19:57:48 +0200

    Update oled.py

--- oled.py
+++ oled.py
@@ -31,7 +31,7 @@ top = padding
 bottom = height - padding
 x = 0
 
-while True:
+def resources():
     draw.rectangle((0,0,width,height), outline=0, fill=0)
     cmd = "hostname -I | cut -d\' \' -f1" 
     ip = subprocess.check_output(cmd, shell=True)
@@ -39,10 +39,10 @@ while True:
     cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
     cpu = subprocess.check_output(cmd, shell=True)
 
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3, $2, $3*100/$2 }'"
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB\", $3, $2, $3*100/$2 }'"
     mem = subprocess.check_output(cmd, shell=True)
 
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3, $2, $5}'"
+    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB\", $3, $2, $5}'"
     disk = subprocess.check_output(cmd, shell=True)
 
     cmd = "vcgencmd measure_temp"
@@ -55,4 +55,17 @@ while True:
 
     oled.image(image)
     oled.display()
+    time.sleep(2)
+
+def datetime():
+    cmd = "date '+%a %d %b %Y'"
+    date = subprocess.check_output(cmd, shell=True)
+    draw.text((x, top), "{}".format(date.decode('utf-8')), font=font, fill=255)
     time.sleep(3)
+
+while True:
+    for _ in range(5):
+        resources()
+    datetime()
+    
+    
