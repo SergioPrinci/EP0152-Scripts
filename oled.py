0a23e00519b5d8b0fe8be92680cc714a05838c69
SergioPrinci <sergioprinci03@live.com>
2022-04-26 15:19:49 +0100

    Second screen now contains uptime and hour:minute, made string management easier

--- oled.py
+++ oled.py
@@ -29,26 +29,28 @@ font = ImageFont.load_default()
 padding = -2
 top = padding
 bottom = height - padding
-x = 0
+x = 2
 
 def resources():
     draw.rectangle((0,0,width,height), outline=0, fill=0)
     cmd = "hostname -I | cut -d\' \' -f1"
-    ip = subprocess.check_output(cmd, shell=True)
+    ip = subprocess.check_output(cmd, shell=True).decode('utf-8')
+    ip = "Ip: " + ip
 
     cmd = """top -bn1 | grep load | awk '{printf "CPU Load: %.2f%%", $(NF-2)}'"""
-    cpu = subprocess.check_output(cmd, shell=True)
+    cpu = subprocess.check_output(cmd, shell=True).decode('utf-8')
 
     cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB\", $3, $2, $3*100/$2 }'"
-    mem = subprocess.check_output(cmd, shell=True)
+    mem = subprocess.check_output(cmd, shell=True).decode('utf-8')
 
     cmd = "vcgencmd measure_temp"
-    temp = subprocess.check_output(cmd, shell=True)
+    temp = subprocess.check_output(cmd, shell=True).decode('utf-8')
+    temp = temp.replace("temp=", "Temp: ")
 
-    draw.text((x, top), "IP: {}".format(ip.decode('utf-8')), font=font, fill=255)
-    draw.text((x, top+8), "{}".format(cpu.decode('utf-8')), font=font, fill=255)
-    draw.text((x, top+16), "{}".format(mem.decode('utf-8')), font=font, fill=255)
-    draw.text((x, top+25), "{}".format(temp.decode('utf-8')), font=font, fill=255)
+    draw.text((x, top), "{}".format(ip), font=font, fill=255)
+    draw.text((x, top+8), "{}".format(cpu), font=font, fill=255)
+    draw.text((x, top+16), "{}".format(mem), font=font, fill=255)
+    draw.text((x, top+25), "{}".format(temp), font=font, fill=255)
 
     oled.image(image)
     oled.display()
@@ -56,10 +58,21 @@ def resources():
 
 def datetime():
     draw.rectangle((0,0,width,height), outline=0, fill=0)
-    cmd = "date '+%a %d %b %Y'"
-    date = subprocess.check_output(cmd, shell=True)
 
-    draw.text((x+16, top+14), "{}".format(date.decode('utf-8')), font=font, fill=255)
+    cmd = "date '+%a %d %b %Y'"
+    date = subprocess.check_output(cmd, shell=True).decode('utf-8')
+    cmd = "date '+%H:%m'"
+    hour = subprocess.check_output(cmd, shell=True).decode('utf-8')
+    cmd = "uptime -p"
+    uptime = subprocess.check_output(cmd, shell=True).decode('utf-8')
+    uptime = uptime.replace("up", "")
+
+    draw.text((x+18, top+4), "{}".format(date), font=font, fill=255)
+    draw.text((x+44, top+12), "{}".format(hour), font=font, fill=255)
+    if uptime.__len__() > 12:
+        draw.text((x, top+20), "Up:{}".format(uptime), font=font, fill=255)
+    else:
+        draw.text((x+12, top+20), "Uptime:{}".format(uptime), font=font, fill=255)
 
     oled.image(image)
     oled.display()
