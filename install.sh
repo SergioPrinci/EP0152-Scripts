2603dc3e3dfdcd998a6091320a4c29a0cbf820f6
sergioprinci <30233283+SergioPrinci@users.noreply.github.com>
2025-09-24 23:33:55 +0200

    Update install.sh
    FINALLY fixed folders

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-echo "Script to install oled and leds python scripts."
+echo "Script to install leds.py and oled.py on the board."
 
 echo "Updating repos..."
 apt -qq update && apt -qq upgrade
 
 echo "Installing or updating dependencies..."
 apt -qq install build-essential python3-dev python3-pip python3-numpy raspi-config -y
-
-tmpFolder="/tmp"
-cd $tmpFolder
 
 if [ "$EUID" -ne 0 ]
 then
@@ -44,28 +41,27 @@ fi
 echo "Installing Python libraries through apt..."
 apt -qq install python3-setuptools python3-wheel \
         python3-smbus python3-pil -y
+
 echo "Installing Python libraries through pip... (no other alternatives!)"
 pip3 install pi-ina219 adafruit-circuitpython-ssd1306 Adafruit-Blinka -q --break-system-package --disable-pip-version-check --root-user-action=ignore
+
 echo "Installing Adafruit_BBIO by compiling from source..."
 git clone https://github.com/adafruit/adafruit-beaglebone-io-python.git
-repoFolder=$(pwd)
 cd adafruit-beaglebone-io-python
 pip install .
-cd $repoFolder
+cd ..
 
 echo "Moving scripts..."
 mkdir $HOME/Scripts
 mv *.py $HOME/Scripts
 
-
 echo "Appending edits to rc.local..."
-echo -e "#!/bin/sh -e\npython3 $HOME/Scripts/leds.py\npython3 $HOME/Scripts/oled.py\nexit" >> /etc/rc.local
+echo -e "#!/bin/bash -e\npython3 $HOME/Scripts/leds.py\npython3 $HOME/Scripts/oled.py\nexit" >> /etc/rc.local
 if [ -x /etc/rc.local ]; then 
         chmod +x /etc/rc.local
 fi
 
-echo "All done!"
-echo "Can I reboot your machine? (y/n)"
+echo "All done! Can I reboot your machine? (y/n)"
 read;
 if [ $REPLY == "y" ]
 then
